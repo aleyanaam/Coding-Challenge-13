c427cf983532bd0c1a3837add0d6f73b860c4d63
Aleyana McLeod <aleyanam@usf.edu>
2024-11-03 15:43:29 -0500

    Format product information dynamically.

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-fetch('https://www.course-api.com/javascript-store-products') //Gets the product from the API
+fetch('https://www.course-api.com/javascript-store-products') //Gets the products from the API
    .then(response => response.json()) 
-   .then(data => displayProducts(data))//Calls the function to display products, passing the fetched data
+   .then(data => displayProducts(data)) 
    .catch(error => {
-    //Handles any errors that occur 
-       console.error('Unable to fetch products, try again later.:', error); 
-       document.getElementById('productContainer').innerHTML = '<p> Unable to load products. Please try again later.</p>';
+    //Handles any errors that may occur
+       console.error('Error! Unable to fetch products:', error); 
+       document.getElementById('productContainer').innerHTML = '<p>Unable to load products. Please try again later.</p>';
    });
+
+function displayProducts(products) {
+    const productContainer = document.getElementById('productContainer');
+    productContainer.innerHTML = ''; 
+ 
+ //Iterates over each product in the array
+    products.forEach(product => { //Searches through for each product
+        const productElement = document.createElement('div');
+        productElement.classList.add('product');
+ 
+        productElement.innerHTML = `
+            <img src="${product.fields.image[0].url}" alt="${product.fields.name}">
+            <h3>${product.fields.name}</h3>
+            <p>${product.fields.company}</p>
+            <p>$${product.fields.price / 100}</p>
+        `;
+ 
+        //Appends the product element to the product container
+        productContainer.appendChild(productElement); 
+    });
+ }
+ 
